ef86340584013c59fbf9c1bb5b504303ee14d8fb
Willy Boulhol <wboulhol@gmail.com>
2024-01-24 16:55:50 +0100

    add sql

--- BDD_SQL/spotify.sql
+++ BDD_SQL/spotify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Jan 22, 2024 at 02:46 PM
+-- Generation Time: Jan 24, 2024 at 03:55 PM
 -- Server version: 10.6.12-MariaDB-1:10.6.12+maria~ubu2004-log
 -- PHP Version: 8.1.14
 
@@ -30,8 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `albums` (
   `id` int(11) NOT NULL,
   `name_album` varchar(255) NOT NULL,
+  `name_artist` varchar(255) NOT NULL,
   `image_album` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `albums`
+--
+
+INSERT INTO `albums` (`id`, `name_album`, `name_artist`, `image_album`) VALUES
+(3, 'Mutter', 'Rammstein', './pochette/mutter.jpg'),
+(5, 'Legend', 'Bob marley', './pochette/bob.jpeg'),
+(7, 'Gods & Nightmares', 'Hollow Knight', './pochette/hollow.jpeg');
 
 -- --------------------------------------------------------
 
@@ -45,6 +55,13 @@ CREATE TABLE `playlist` (
   `id_users` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `playlist`
+--
+
+INSERT INTO `playlist` (`id`, `name_playslist`, `id_users`) VALUES
+(3, 'Random', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -55,9 +72,25 @@ CREATE TABLE `titres` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `id_album` int(11) NOT NULL,
-  `id_playlist` int(11) NOT NULL,
+  `id_playlist` int(11) DEFAULT NULL,
   `link` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `titres`
+--
+
+INSERT INTO `titres` (`id`, `name`, `id_album`, `id_playlist`, `link`) VALUES
+(8, 'Sonne', 3, 3, './mp3/sonne.mp3'),
+(10, 'Amerika', 3, 3, './mp3/amerikaramm.mp3'),
+(12, 'Du Hast', 3, 3, './mp3/duhastramm.mp3'),
+(14, 'Ich will', 3, 3, './mp3/duhastramm.mp3'),
+(16, 'One love', 5, 3, './mp3/onelove.mp3'),
+(17, 'Don\'t Worry', 5, 3, './mp3/worry.mp3'),
+(18, 'Buffalo soldiers', 5, 3, './mp3/buffalo.mp3'),
+(22, 'Dream Battle', 7, 3, './mp3/dreambattle.mp3'),
+(23, 'The Grimm Troupe', 7, 3, './mp3/thegrimmtroupe.mp3'),
+(24, 'Nightmare King', 7, 3, './mp3/nightmaresking.mp3');
 
 -- --------------------------------------------------------
 
@@ -68,8 +101,15 @@ CREATE TABLE `titres` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `pseudo` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `pseudo`, `password`) VALUES
+(1, 'willy', '$2y$10$ayDsUoz5XU4VqHAeQ.2Pou8.7Le3Eruqy435pA0cXCwsuK.xwoRLe');
 
 --
 -- Indexes for dumped tables
@@ -93,8 +133,8 @@ ALTER TABLE `playlist`
 --
 ALTER TABLE `titres`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_playlist` (`id_playlist`),
-  ADD KEY `id_album` (`id_album`);
+  ADD KEY `id_album` (`id_album`),
+  ADD KEY `id_playlist` (`id_playlist`);
 
 --
 -- Indexes for table `users`
@@ -110,25 +150,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `albums`
 --
 ALTER TABLE `albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `playlist`
 --
 ALTER TABLE `playlist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `titres`
 --
 ALTER TABLE `titres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -144,8 +184,8 @@ ALTER TABLE `playlist`
 -- Constraints for table `titres`
 --
 ALTER TABLE `titres`
-  ADD CONSTRAINT `titres_ibfk_1` FOREIGN KEY (`id_playlist`) REFERENCES `playlist` (`id`),
-  ADD CONSTRAINT `titres_ibfk_2` FOREIGN KEY (`id_album`) REFERENCES `albums` (`id`);
+  ADD CONSTRAINT `titres_ibfk_1` FOREIGN KEY (`id_album`) REFERENCES `albums` (`id`),
+  ADD CONSTRAINT `titres_ibfk_2` FOREIGN KEY (`id_playlist`) REFERENCES `playlist` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
